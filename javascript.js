7c67e661df9dba25c3b74dfebd101b886f9679da
juani-ml <juanymartineez@gmail.com>
2023-04-22 16:38:05 -0300

    completed lesson 1

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
 console.log("Hello from another file!")
+
+let a = 10
+console.log(a)
+
+console.log(a * 90)
+
+let b = 7 * a
+console.log(b)
+
+const MAX = 57
+
+let actual = MAX - 13
+
+let percentage = actual / MAX
+
+console.log(percentage)
