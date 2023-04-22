c4d502021a56cbe81e6cb5739f04a706c74f3788
juani-ml <juanymartineez@gmail.com>
2023-04-22 16:30:12 -0300

    added task one

--- variables.js
+++ variables.js
@@ -6,3 +6,8 @@ alert(message); // shows the variable contents
 let message2 = 'Hello!'; // define the variable and assign the value
 
 let user = 'John', age = 25, message3 = 'Hello'; //multiple declarations on one line
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
