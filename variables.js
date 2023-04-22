32765706623e8d63a1f2bef264ca9507fa059087
juani-ml <juanymartineez@gmail.com>
2023-04-22 16:10:39 -0300

    project setup

---
+++ variables.js
@@ -0,0 +1,8 @@
+let message;
+
+message = 'Hello'
+alert(message); // shows the variable contents
+
+let message2 = 'Hello!'; // define the variable and assign the value
+
+let user = 'John', age = 25, message3 = 'Hello'; //multiple declarations on one line
