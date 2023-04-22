a00fc3488ef1dfca9341c9e116ef1a16c4c6152d
juani-ml <juanymartineez@gmail.com>
2023-04-22 16:32:39 -0300

    added remaining tasks

--- tasks.js
+++ tasks.js
@@ -5,3 +5,10 @@ username = "John"
 admin = username
 
 alert(admin)
+
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+
+const birthday = '18.04.1982';
+
+const age = someCode(birthday);
